78c42386a37d41829cf998178d4dd19d29cac515
JayClot <clotworthyjc@gmail.com>
2021-02-19 21:06:54 +0000

    break homepage in to components, finish styling menu items

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import MenuItem from "../../components/menu-item/menu-item.component";
+import "./directory.styles.scss";
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: "Sofas",
+          imageUrl:
+            "https://d2kz53n3bzvihv.cloudfront.net/resized/790_395_0_255_255_255/media/gbu0/categoryxxl/image/Desktop-Header_1000x500px-Sofas.jpg.jpeg",
+          id: 1,
+        },
+        {
+          title: "Tables",
+          imageUrl:
+            "https://casefurniture.com/wp-content/uploads/contemporary-dulwich-table-matthew-hilton-case-furniture-category-landscape.jpg",
+          id: 2,
+        },
+        {
+          title: "Wardrobes",
+          imageUrl:
+            "https://www.cuckooland.com/dnc/cuckooland//artwork/product_images/All-Black-Simple-4-Door-Wardrobe.jpg?quality=90&scale=canvas&width=1000&height=1000",
+          id: 3,
+        },
+        {
+          title: "Bookshelves",
+          imageUrl:
+            "https://spy.com/wp-content/uploads/2019/11/floating-shelves-1.jpg?w=958&h=599&crop=1",
+          size: "large",
+          id: 4,
+        },
+        {
+          title: "Desks",
+          imageUrl:
+            "https://www.windowscentral.com/sites/wpcentral.com/files/styles/w1600h900crop/public/field/image/2019/12/best-computer-desk-hero.jpg",
+          size: "large",
+          id: 5,
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
