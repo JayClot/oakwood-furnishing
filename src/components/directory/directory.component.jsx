0ad2a02722756ac880a78a321b3bad35fe9dfb67
JayClot <clotworthyjc@gmail.com>
2021-02-20 18:45:46 +0000

    add routing to site

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,24 +12,28 @@ class Directory extends React.Component {
           title: "Sofas",
           imageUrl:
             "https://d2kz53n3bzvihv.cloudfront.net/resized/790_395_0_255_255_255/media/gbu0/categoryxxl/image/Desktop-Header_1000x500px-Sofas.jpg.jpeg",
+          linkUrl: "sofas",
           id: 1,
         },
         {
           title: "Tables",
           imageUrl:
             "https://casefurniture.com/wp-content/uploads/contemporary-dulwich-table-matthew-hilton-case-furniture-category-landscape.jpg",
+          linkUrl: "tables",
           id: 2,
         },
         {
           title: "Wardrobes",
           imageUrl:
             "https://www.cuckooland.com/dnc/cuckooland//artwork/product_images/All-Black-Simple-4-Door-Wardrobe.jpg?quality=90&scale=canvas&width=1000&height=1000",
+          linkUrl: "wardrobes",
           id: 3,
         },
         {
           title: "Bookshelves",
           imageUrl:
             "https://spy.com/wp-content/uploads/2019/11/floating-shelves-1.jpg?w=958&h=599&crop=1",
+          linkUrl: "bookshelves",
           size: "large",
           id: 4,
         },
@@ -37,6 +41,7 @@ class Directory extends React.Component {
           title: "Desks",
           imageUrl:
             "https://www.windowscentral.com/sites/wpcentral.com/files/styles/w1600h900crop/public/field/image/2019/12/best-computer-desk-hero.jpg",
+          linkUrl: "desks",
           size: "large",
           id: 5,
         },
@@ -47,8 +52,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...sectionProps }) => (
+          <MenuItem key={id} {...sectionProps} />
         ))}
       </div>
     );
